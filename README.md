306aa7b5bee0caad5292587cd3eda004d32efa76
Hunter Haydel <hhaydel@gmail.com>
2018-07-21 12:07:03 -0400

    style popup

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 # grocery-list
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn run build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn run lint
 ```
+
+## Credits
+
+Icons made by [Freepik](http://www.freepik.com) from [Flaticon](https://www.flaticon.com/) is licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/)
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,23 +1,34 @@
 <template>
-  <div id="app">
-    <label for="servings">Servings</label>
-    <input name="servings" v-model="servings" />
-    <div>
-      <div v-for="(recipe, index) in recipes" :key="recipe.url" class="recipe">
-        <button @click="deleteIndex(index)">Delete</button>
-        <div class="recipe-name">{{recipe.name}}</div>
-        <a :href="recipe.url" class="recipe-url">{{recipe.url}}</a>
-      </div>
+  <div class="popup">
+    <div class="servings">
+      <label for="servings" class="servings-label">Servings</label>
+      <input type="number" name="servings" class="validate servings-input" v-model="servings" />
     </div>
-    <button @click="handleAdd">Add Current Page</button>
-    <div v-if="error" class="error">{{error.message}}</div>
-    <div v-if="ingredients">{{ingredients}}</div>
-    <button @click="handleGroceryList">Grocery List</button>
+    <ul class="collection recipies">
+      <li v-for="(recipe, index) in recipes" :key="recipe.url" class="collection-item recipe">
+        <div>
+          <a :href="recipe.url" class="recipe-name" target="_blank">
+            {{recipe.name}}
+          </a>
+          <button @click="deleteIndex(index)" class="secondary-content btn btn-small waves-effect waves-light red recipie-delete">
+            <i class="material-icons">delete</i>
+          </button>
+        </div>
+      </li>
+    </ul>
+    <div v-if="error" class="red-text error">{{error.message}}</div>
+    <button @click="handleAdd" class="btn-floating btn-small waves-effect waves-light">
+      <i class="material-icons">add</i>
+    </button>
+    <button @click="handleGroceryList" class="btn right btn-small waves-effect waves-light build-grocery-list">
+      Grocery List
+    </button>
   </div>
 </template>
 
 <script>
 /* global chrome */
+import 'materialize-css/dist/css/materialize.min.css'
 import * as Recipes from '@/recipes'
 const RECIPES_KEY = 'recipes'
 
@@ -38,7 +49,6 @@ export default {
       servings: 2,
       recipes: [],
       error: null,
-      ingredients: null
     }
   },
   methods: {
@@ -74,12 +84,8 @@ export default {
 </script>
 
 <style>
-.recipe {
-}
-
-.recipe-url {
-}
-
-.error {
+.popup {
+  min-width: 500px;
+  padding: 5px;
 }
 </style>
