4f6abfc0d8aadc0544bcc20b32062503f3092c42
Hunter Haydel <hhaydel@gmail.com>
2018-07-21 17:21:38 -0400

    add empty message

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -4,21 +4,25 @@
       <label for="servings" class="servings-label">Servings</label>
       <input type="number" name="servings" class="validate servings-input" v-model="servings" />
     </div>
-    <ul class="collection recipies">
+    <ul v-if="recipes.length" class="collection recipies">
       <li v-for="(recipe, index) in recipes" :key="recipe.url" class="collection-item recipe">
         <a :href="recipe.url" class="recipe-name" target="_blank">
           {{recipe.name}}
         </a>
-        <button @click="deleteIndex(index)" class="btn btn-small waves-effect waves-light red recipie-delete">
+        <button @click="deleteIndex(index)" class="btn btn-small waves-effect waves-light red recipe-delete">
           <i class="material-icons">delete</i>
         </button>
       </li>
     </ul>
+    <div v-else class="empty-list">
+      <div>There are currently no recipes added. Click the + below to add a recipe from the current page.</div>
+      <i class="material-icons">fastfood</i>
+    </div>
     <div v-if="error" class="red-text error">{{error.message}}</div>
     <button @click="handleAdd" class="btn-floating btn-small waves-effect waves-light">
       <i class="material-icons">add</i>
     </button>
-    <button @click="handleGroceryList" class="btn right btn-small waves-effect waves-light build-grocery-list">
+    <button @click="handleGroceryList" class="btn right btn-small waves-effect waves-light build-grocery-list" :disabled="!recipes.length">
       Grocery List
     </button>
   </div>
@@ -74,11 +78,16 @@ export default {
 <style>
 .popup {
   min-width: 500px;
-  padding: 5px;
+  padding: 15px;
 }
 
 .recipe {
   display: flex;
   justify-content: space-between;
+}
+
+.empty-list {
+  text-align: center;
+  margin: 10px;
 }
 </style>
